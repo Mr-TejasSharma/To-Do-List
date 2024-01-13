81ecbabfd0be480590f4cfd7ec53312170eb1a11
Mr-TejasSharma <tejas.sharma111002@gmail.com>
2024-01-13 19:22:49 +0530

    Added Local Storage

---
+++ script.js
@@ -0,0 +1,31 @@
+var ToDo=JSON.parse(localStorage.getItem("TODO"))||[""];
+const List= document.querySelector(".todo");
+const List_Item = document.querySelector(".data");
+const Add=document.querySelector("#button")
+const Clear=document.querySelector(".clear")
+
+Add.addEventListener("click",function(){
+    AddItem(List_Item.value)
+})
+function AddItem(item){
+    if(item)
+    {    
+    ToDo.push(item);
+    makelist();
+    }
+    localStorage.setItem("TODO",JSON.stringify(ToDo));
+};
+function makelist(){
+    var m=[];
+    for (i=ToDo.length-1;i>=0;i--)
+    {
+        m[ToDo.length-i]='<li>'+ToDo[i]+'</li>';
+    } 
+    List.innerHTML='<ul>'+m.join('')+'</ul>';
+}
+makelist();
+
+Clear.addEventListener("click",function(){
+    localStorage.clear();
+    makelist();
+})
