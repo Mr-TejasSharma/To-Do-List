f9acb367fed8d55d1b9b9e202717c62cf8d318ac
Mr-TejasSharma <tejas.sharma111002@gmail.com>
2024-01-17 10:04:35 +0530

    ADDED Check Box

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-var ToDo=JSON.parse(localStorage.getItem("TODO"))||[""];
-const List= document.querySelector(".todo");
+var ToDo = JSON.parse(localStorage.getItem("TODO")) || [];
+const List = document.querySelector(".todo");
 const List_Item = document.querySelector(".data");
-const Add=document.querySelector("#button")
-const Clear=document.querySelector(".clear")
+const Add = document.querySelector("#button");
+const Clear = document.querySelector(".clear");
 
-Add.addEventListener("click",function(){
-    AddItem(List_Item.value)
-})
-function AddItem(item){
-    if(item)
-    {    
+Add.addEventListener("click", function () {
+  AddItem(List_Item.value);
+});
+function AddItem(item) {
+  if (item) {
     ToDo.push(item);
     makelist();
-    }
-    localStorage.setItem("TODO",JSON.stringify(ToDo));
-};
-function makelist(){
-    var m=[];
-    for (i=ToDo.length-1;i>=0;i--)
-    {
-        m[ToDo.length-i]='<li>'+ToDo[i]+'</li>';
-    } 
-    List.innerHTML='<ul>'+m.join('')+'</ul>';
+  }
+  localStorage.setItem("TODO", JSON.stringify(ToDo));
+}
+function makelist() {
+  var m = [];
+  for (i = ToDo.length - 1; i >= 0; i--) {
+    m[ToDo.length - i] =
+      `<li class= item${i} ><input type="checkbox" class="box" ><span class="work">` +
+      ToDo[i] +
+      "</span></li>";
+  }
+  List.innerHTML = "<ul>" + m.join("") + "</ul>";
 }
 makelist();
 
-Clear.addEventListener("click",function(){
-    localStorage.clear();
-    makelist();
-})
+Clear.addEventListener("click", function () {
+  localStorage.clear();
+  makelist();
+});
